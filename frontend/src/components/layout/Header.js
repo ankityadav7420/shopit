1efc53d8c5ebf79f85c03630624fd59a75292f9d
ankityadav7420 <kumar2001y@gmail.com>
2023-09-14 12:57:08 +0530

    Completed cart adding and genral calculation

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
   const navigate= useNavigate();
   const dispatch = useDispatch();
   const {user,loading}  = useSelector(state=>state.auth)
-
+  const {cartItems} = useSelector(state=>state.cart)
   const logoutHandler=()=>{
     dispatch(logout());
     alert("user logout seuccesfully")
@@ -37,17 +37,21 @@ const Header = () => {
       <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
         <Link to="/cart" style={{textDecoration:'none'}}>
             <span id="cart" className="ml-3">Cart</span>
-            <span className="ml-1" id="cart_count">2</span>
+            <span className="ml-1" id="cart_count">{cartItems?.length}</span>
         </Link>
         {user ? (
             <div className='ml-4 dropdown d-inline'>
                 <Link to="#!" className="btn dropdown-toggle text-white mr-4" type='button' 
                 id="dropDownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <figure className='avatar avatar-nav'> 
-                    <img src={ `'/images/default_avatar.jpg' || ${user.avatar && user.avatar.url}`}
-                    //  alt={user && user.name}
+                    <img src={'/images/default_avatar.jpg'}
+                     alt="user"
                     className='rounded-circle' 
                     />
+                     {/* <img src={ ` ${user.avatar && user.avatar.url}`}
+                     alt={user && user.name}
+                    className='rounded-circle' 
+                    /> */}
                   </figure>
                   <span> {user && user.name}</span>
                 </Link>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,7 +5,7 @@ import {createStore, combineReducers, applyMiddleware } from 'redux';
 import {productReducers, productDetailsReducer} from './reducers/productReducers';
 
 import{authReducer, userReducer, forgotPasswordReducer} from './reducers/userReducer'
-
+import { cartReducer } from './reducers/cartReducers';
 
 
 const reducer = combineReducers({
@@ -13,11 +13,18 @@ const reducer = combineReducers({
     productDetails: productDetailsReducer,
     auth:authReducer,
     user:userReducer,
-    forgotPassword: forgotPasswordReducer
+    forgotPassword: forgotPasswordReducer,
+    cart:cartReducer
 })
 
 const middlware = [thunk];
-let initialState = {}
+let initialState = {
+    cart:{
+        cartItems: localStorage.getItem('cartItems')
+        ? JSON.parse(localStorage.getItem('cartItems'))
+        : []
+    }
+}
 
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middlware)))
 
